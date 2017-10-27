ae10305f3fb64db2bac9361ecedee0b38ef5e648
dangphu1993 <dangphu.56pm@gmail.com>
2017-10-27 08:23:32 +0700

    temp commit

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from '../models/User';
+import parseErrors from '../utils/parseErrors';
 
 const router = express.Router();
 
@@ -8,7 +9,8 @@ router.post('/', (req, res) => {
   const user = new User({ email });
   user.setPassword(password);
   user.save()
-    .catch(err => res.json({ err }));
+    .then(userRecord => res.json({ user: userRecord.toAuthJSON() }))
+    .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
 });
 
 export default router;
