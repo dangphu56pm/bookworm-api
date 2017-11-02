9792143785258ff92088a4c08d9ae18f1d9cf574
phund7 <phund7@fpt.com.vn>
2017-11-02 19:55:01 +0700

    temp commit

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import User from '../models/User';
 import parseErrors from '../utils/parseErrors';
+import { sendResetPasswordRequest } from '../mailer';
 
 const router = express.Router();
 
@@ -25,6 +27,27 @@ router.post('/confirmation', (req, res) => {
     res.json({ user: user.toAuthJSON() });
   })
     .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
+});
+
+router.post('/reset_password_request', (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      sendResetPasswordRequest(user);
+      res.json({});
+    } else {
+      res.status(400).json({ errors: { global: 'There is no user with such email ' } });
+    }
+  });
+});
+
+router.post('/validate_token', (req, res) => {
+  jwt.verify(req.body.token, process.env.JWT_SECRET, (err) => {
+    if (err) {
+      res.stats(401).json({});
+    } else {
+      res.json({});
+    }
+  });
 });
 
 export default router;
